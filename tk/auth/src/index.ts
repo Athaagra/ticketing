4579f1864f1f23c5bf928e63011fbcee94ff07ac
Kel3vra <46803277+Athaagra@users.noreply.github.com>
2025-12-02 19:50:05 +0200

    Update index.ts

--- tk/auth/src/index.ts
+++ tk/auth/src/index.ts
@@ -1,9 +1,41 @@
+import { signoutRouter } from ' ./routes/signout';
+import { signupRouter } from ' ./routes/signup';
+
 import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
 app.use(bodyParser.json());
 
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.all('*', async (req,res) => {
+	theow new NotFoundError();
+});
+
+app.use(errorHandler);
+
+
+const start = () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth',{
+			userNewUrlParser: true,
+			userUnifiedTopology: true,
+			uaeCreateIndex: true
+		});
+		console.log('connected to Mongodb');
+	} catch(err){
+		console.error(err);
+	}
+
+
 app.listen(3000, () => {
 	console.log("Listening on port 3000!");
-});
+ });
+};
+
+start();
