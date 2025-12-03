8ffb55b41eec2f18fa0cb4d1bfb8fb3d53ad17da
athaagra <athaagrak@gmail.com>
2025-12-03 11:07:18 +0200

    Error problem

---
+++ index.ts
@@ -0,0 +1,47 @@
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { signinRouter } from './routes/signin';
+import { currentUserRouter} from './routes/current-user';
+import { errorHandler } from './middleware/error-handler';
+//import { NotFoundError } from './errors/not-found-error'; 
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import 'express-async-errors';
+import mongoose from 'mongoose';
+const app = express();
+app.use(bodyParser.json());
+
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.all('*', async (req,res) => {
+	//throw new NotFoundError();
+	console.log('Errror');
+});
+
+//app.use(errorHandler);
+
+
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth',{
+			userNewUrlParser: true,
+			userUnifiedTopology: true,
+			uaeCreateIndex: true
+		});
+		console.log('connected to Mongodb');
+	} catch(err){
+		console.error(err);
+	}
+
+
+app.listen(3000, () => {
+	console.log("Listening on port 3000!");
+ });
+};
+
+start();
