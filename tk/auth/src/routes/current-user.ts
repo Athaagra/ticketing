aee7162a2ddf707725e3c7f3fa69bde6ab41fb53
Kel3vra <46803277+Athaagra@users.noreply.github.com>
2025-12-20 01:04:30 +0200

    Update current-user.ts

--- tk/auth/src/routes/current-user.ts
+++ tk/auth/src/routes/current-user.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import { currentUser } from '../middleware/current-user';
 import { Request, Response, NextFunction } from 'express';
-
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, ( res,req) => {
-    //res.send({ currentUser: req.currentUser });
+    if(!req.session?.jwt){
+        return res.send({currentUser: null})
+    }
+    try{
+        const payload = jwt.verify(req.session.jwt,
+                                   process.env.JWT!
+        );
+    }
+    catch(err){
+    res.send({ currentUser: null});
+    }
 });
 
 export { router as currentUserRouter };
